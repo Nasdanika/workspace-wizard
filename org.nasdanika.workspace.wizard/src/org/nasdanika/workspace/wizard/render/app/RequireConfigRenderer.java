38ce7143008e75b9a14e748726914806a53e45fc
Pavel <Pavel.Vlasov@nasdanika.org>
2017-04-12 20:34:01 -0400

    merge

--- org.nasdanika.workspace.wizard/src/org/nasdanika/workspace/wizard/render/app/RequireConfigRenderer.java
+++ org.nasdanika.workspace.wizard/src/org/nasdanika/workspace/wizard/render/app/RequireConfigRenderer.java
@@ -13,12 +13,14 @@ public class RequireConfigRenderer {
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "require.config({" + NL + "    baseUrl: '{{base-url}}'," + NL + "    paths: {" + NL + "        jquery: 'jquery-global', " + NL + "        knockout: 'knockout-3.4.0'" + NL + "    }" + NL + "});";
+  protected final String TEXT_1 = "require.config({" + NL + "    baseUrl: '{{base-url}}'," + NL + "    paths: {" + NL + "        jquery: 'jquery-global', " + NL + "    }" + NL + "});";
+  protected final String TEXT_2 = NL;
 
 public String generate(org.nasdanika.workspace.wizard.WorkspaceWizard wizard) throws Exception
   {
     final StringBuffer stringBuffer = new StringBuffer();
     stringBuffer.append(TEXT_1);
+    stringBuffer.append(TEXT_2);
     return stringBuffer.toString();
   }
 }
